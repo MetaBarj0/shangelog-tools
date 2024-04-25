052b24d23d33dad74e67a9e01e43250c8c6aafb0
sebastien.levy <sebastien.levy@ringover.com>
2024-04-25 15:47:16 +0200

    refactor(generate): functions
    Exported all implementation details (inner functions) into a specific
    file. It'll be useful to test-drive complex algorithm outside the main
    feature of the changelog generation.

--- src/generate.sh
+++ src/generate.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
-load_strings() {
+get_script_dir() {
   local script_dirname="$(dirname "$0")"
   cd "$script_dirname" >/dev/null 2>&1
-  local script_dir="$(pwd -P)"
+  pwd -P
   cd - >/dev/null 2>&1
-
-  source "${script_dir}/generate.sh.d/strings.sh"
 }
 
-initialize_argument_default_values() {
-  initial_version=v0.1.0
+load_functions() {
+  source "$(get_script_dir)/generate.sh.d/functions.sh"
 }
 
-ensure_arguments_are_valid() {
-  echo "$initial_version" | pcregrep "${generate_semver_regex}" > /dev/null
+load_strings() {
+  source "$(get_script_dir)/generate.sh.d/strings.sh"
 }
 
 parse_arguments() {
@@ -54,19 +52,6 @@ change_current_directory() {
   fi
 }
 
-ensure_current_directory_is_git_repository() {
-  git status > /dev/null 2>&1
-}
-
-ensure_script_is_within_git_repository() {
-  if [ $? -ne 0 ]; then
-    local script_dirname="$(dirname "$0")"
-    cd "$script_dirname" >/dev/null 2>&1
-  fi
-
-  ensure_current_directory_is_git_repository
-}
-
 ensure_targeting_git_repository() {
   ensure_current_directory_is_git_repository \
   || ensure_script_is_within_git_repository
@@ -95,12 +80,6 @@ ensure_there_are_no_pending_changes() {
   fi
 }
 
-list_changelog_compliant_commits() {
-  git rev-list \
-    --all -E -i --grep \
-    "^(${generate_conventional_commit_type_regex})(\(.+\))?: [^ ].*"
-}
-
 ensure_there_are_at_least_one_conventional_commit() {
   local changelog_compliant_commits="$(list_changelog_compliant_commits)"
 
@@ -110,83 +89,6 @@ ensure_there_are_at_least_one_conventional_commit() {
   fi
 
   echo "${changelog_compliant_commits}"
-}
-
-generate_commit_type_header() {
-  cat << EOF
-### $1
-EOF
-}
-
-generate_commit_type_content_for() {
-  local commit_type="$1"
-  local changelog_compliant_commits="$2"
-  local commit_lines=
-  local commit_sha1=
-
-  while read commit_sha1; do
-    local commit_summary="$(git show -s --pretty='format:%s' $commit_sha1)"
-    local conventional_commit_header='^('"${commit_type}"')(\(.+\))?: ([^ ].*)'
-    local sha1="$(echo ${commit_sha1} | cut -c 0-8)"
-    local commit_line="$( \
-      echo $commit_summary \
-      | grep -E \
-        "${conventional_commit_header}" \
-      | sed -E \
-        's/'"${conventional_commit_header}"'/- \2 \3 ['"${sha1}"']/' \
-      | sed -E \
-        's/-  (.+)/- \1/'
-    )"
-
-    commit_lines="$(cat << EOF
-${commit_lines}
-${commit_line}
-EOF
-    )"
-  done << EOF
-${changelog_compliant_commits}
-EOF
-
-  echo "$commit_lines" | sed -E '/^$/d'
-}
-
-initialize_all_commit_type_variables() {
-  local changelog_compliant_commits="$1"
-
-  while read -d '|' commit_type; do
-    local commit_type_header="$(generate_commit_type_header $commit_type)"
-    local commit_type_content="$(generate_commit_type_content_for $commit_type "${changelog_compliant_commits}")"
-
-    if [ -z "${commit_type_content}" ]; then
-      continue
-    fi
-
-    eval "$(cat << EOF_eval
-${commit_type}_paragraph=\$(cat << EOF
-\${commit_type_header}
-
-\${commit_type_content}
-EOF
-)
-EOF_eval
-    )"
-  done << EOF_while
-$(echo $generate_conventional_commit_type_regex)|
-EOF_while
-}
-
-output_all_commit_type_paragraphs() {
-  while read -d '|' commit_type; do
-    eval "local paragraph=\"\${${commit_type}_paragraph}\""
-
-    if [ -z "${paragraph}" ]; then
-      continue
-    fi
-
-    echo $'\n'"${paragraph}"
-  done << EOF
-$(echo $generate_conventional_commit_type_regex)|
-EOF
 }
 
 bump_version_if_asked() {
@@ -221,7 +123,8 @@ output_changelog() {
 }
 
 main() {
-  load_strings \
+  load_functions \
+  && load_strings \
   && parse_arguments "$@" \
   && change_current_directory "$git_repository_directory" \
   && ensure_targeting_git_repository \
---
+++ src/generate.sh.d/functions.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+initialize_argument_default_values() {
+  initial_version=v0.1.0
+}
+
+ensure_arguments_are_valid() {
+  echo "$initial_version" | pcregrep "${generate_semver_regex}" > /dev/null
+}
+
+ensure_current_directory_is_git_repository() {
+  git status > /dev/null 2>&1
+}
+
+ensure_script_is_within_git_repository() {
+  if [ $? -ne 0 ]; then
+    local script_dirname="$(dirname "$0")"
+    cd "$script_dirname" >/dev/null 2>&1
+  fi
+
+  ensure_current_directory_is_git_repository
+}
+
+list_changelog_compliant_commits() {
+  git rev-list \
+    --all -E -i --grep \
+    "^(${generate_conventional_commit_type_regex})(\(.+\))?: [^ ].*"
+}
+
+generate_commit_type_header() {
+  cat << EOF
+### $1
+EOF
+}
+
+generate_commit_type_content_for() {
+  local commit_type="$1"
+  local changelog_compliant_commits="$2"
+  local commit_lines=
+  local commit_sha1=
+
+  while read commit_sha1; do
+    local commit_summary="$(git show -s --pretty='format:%s' $commit_sha1)"
+    local conventional_commit_header='^('"${commit_type}"')(\(.+\))?: ([^ ].*)'
+    local sha1="$(echo ${commit_sha1} | cut -c 0-8)"
+    local commit_line="$( \
+      echo $commit_summary \
+      | grep -E \
+        "${conventional_commit_header}" \
+      | sed -E \
+        's/'"${conventional_commit_header}"'/- \2 \3 ['"${sha1}"']/' \
+      | sed -E \
+        's/-  (.+)/- \1/'
+    )"
+
+    commit_lines="$(cat << EOF
+${commit_lines}
+${commit_line}
+EOF
+    )"
+  done << EOF
+${changelog_compliant_commits}
+EOF
+
+  echo "$commit_lines" | sed -E '/^$/d'
+}
+
+initialize_all_commit_type_variables() {
+  local changelog_compliant_commits="$1"
+
+  while read -d '|' commit_type; do
+    local commit_type_header="$(generate_commit_type_header $commit_type)"
+    local commit_type_content="$(generate_commit_type_content_for $commit_type "${changelog_compliant_commits}")"
+
+    if [ -z "${commit_type_content}" ]; then
+      continue
+    fi
+
+    eval "$(cat << EOF_eval
+${commit_type}_paragraph=\$(cat << EOF
+\${commit_type_header}
+
+\${commit_type_content}
+EOF
+)
+EOF_eval
+    )"
+  done << EOF_while
+$(echo $generate_conventional_commit_type_regex)|
+EOF_while
+}
+
+output_all_commit_type_paragraphs() {
+  while read -d '|' commit_type; do
+    eval "local paragraph=\"\${${commit_type}_paragraph}\""
+
+    if [ -z "${paragraph}" ]; then
+      continue
+    fi
+
+    echo $'\n'"${paragraph}"
+  done << EOF
+$(echo $generate_conventional_commit_type_regex)|
+EOF
+}
