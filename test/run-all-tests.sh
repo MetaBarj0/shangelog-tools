af4721134d4b8ed23f4bda162fa6b580c8c08e17
sebastien.levy <sebastien.levy@ringover.com>
2024-05-23 09:19:03 +0200

    refactor(tests): runner
    run-all-tests.sh has been refactored a bit. May ease further extensions.

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -1,38 +1,56 @@
 #!/bin/sh
 
-script_dir="$(dirname $0)"
-cd "$script_dir" 2>&1 >/dev/null
-script_dir="$(pwd)"
-cd - 2>&1 >/dev/null
+get_script_dir() {
+  cd "$(dirname $0)" 2>&1 >/dev/null
 
-echo 'Cloning bats submodules...'
-cd "${script_dir}/.." 2>&1 >/dev/null
-git submodule --quiet update --init --recursive
-cd - 2>&1 >/dev/null
+  echo "$(pwd)"
 
-echo 'Building docker test image...'
-docker build \
-  -q \
-  -t shangelog-tools-tester \
-  "${script_dir}/docker.d" \
-  >/dev/null
+  cd - 2>&1 >/dev/null
+}
 
-echo 'Running tests...'
-# https://bats-core.readthedocs.io/en/stable/writing-tests.html#special-variables
-# for explanation about $BATS_TMPDIR and /tmp
-docker run \
-  --init --rm -it \
-  -e TMPDIR=/tmp \
-  -e HOST_TEST_OUTPUT_DIR="${script_dir}/test_output" \
-  -v "${script_dir}/../":/root/ringover-shangelog-tools/:ro \
-  -v /var/run/docker.sock:/var/run/docker.sock:ro \
-  -v "${script_dir}/test_output":/tmp \
-  shangelog-tools-tester "$@"
+prepare_test_environment() {
+  echo 'preparing test environment...'
 
-result=$?
+  cd "$(get_script_dir)/.." 2>&1 >/dev/null
+  git submodule --quiet update --init --recursive
+  cd - 2>&1 >/dev/null
 
-echo 'Cleaning up the mess...'
-docker image rm shangelog-tools-tester >/dev/null
-rm -rf "${script_dir}/test_output/"*
+  docker build \
+    -q \
+    -t shangelog-tools-tester \
+    "$(get_script_dir)/docker.d" \
+    >/dev/null
+}
 
-exit $result
+run_test_suites() {
+  echo 'running tests...'
+
+  # https://bats-core.readthedocs.io/en/stable/writing-tests.html#special-variables
+  # for explanation about $BATS_TMPDIR and /tmp
+  docker run \
+    --init --rm -it \
+    -e TMPDIR=/tmp \
+    -e HOST_TEST_OUTPUT_DIR="$(get_script_dir)/test_output" \
+    -v "$(get_script_dir)/../":/root/ringover-shangelog-tools/:ro \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    -v "$(get_script_dir)/test_output":/tmp \
+    shangelog-tools-tester "$@"
+}
+
+cleanup() {
+  echo 'cleaning up the mess...'
+
+  docker image rm shangelog-tools-tester >/dev/null
+  rm -rf "$(get_script_dir)/test_output/"*
+}
+
+main() {
+  prepare_test_environment \
+  && run_test_suites "$@" \
+  ; local result=$? \
+  && cleanup
+
+  return $result
+}
+
+main "$@"
